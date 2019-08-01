966e4d52136086e6f102201cfcb1718ce6793650
bohrytsenko <hrytsenkobohdan@gmail.com>
2019-08-01 12:53:59 +0200

    Activities, leads, nav, fixes

--- assets/js/scrollto.js
+++ assets/js/scrollto.js
@@ -19,6 +19,31 @@
 	});
 })(jQuery);
 
+// To do list
+
+var counter = 1;
+
+function enter_task () {
+    var text = $('#enter_task').val();
+    $('#todo_list').append('<li class="mb-2"><span>'+ text + '</span><button class="primary add_task__edit" type="button" id="edit' + counter + '"><i class="fa fa-edit fa-lg"></i></button>' + '<button class="primary ml-2 add_task__edit" type="button" class="done" id="delete' + counter + '"><i class="fa fa-remove fa-lg"></i></button>' +'</li>');
+
+    $('#edit' + counter).click(function(){
+        $(this).prev().attr('contenteditable','true');
+        $(this).prev().focus();
+    });
+
+    $('#delete' + counter).click(function(){
+        $(this).parent().remove();
+    });
+
+    counter++;
+};
+
+$(function() {
+    $('#add').on('click', enter_task);
+});
+
+
 $(document).ready(function(){
 
     // $(document).on('click',".modal-open", function(e) {
@@ -53,28 +78,28 @@ $(document).ready(function(){
 
     // Modal show
 
-    $('.intro_show').on('click', function () {
-    	$('.show_bl').hide();
-        $('.intro').fadeIn();
-    });
-
-    $('.resources_show').on('click', function () {
-        $('.show_bl').hide();
-        $('.resources').fadeIn();
-    });
-
-    $('.notification_show').on('click', function () {
-        $('.show_bl').hide();
-        $('.notification').fadeIn();
-    });
+    // $('.intro_show').on('click', function () {
+    // 	$('.show_bl').hide();
+    //     $('.intro').fadeIn();
+    // });
+    //
+    // $('.resources_show').on('click', function () {
+    //     $('.show_bl').hide();
+    //     $('.resources').fadeIn();
+    // });
+    //
+    // $('.notification_show').on('click', function () {
+    //     $('.show_bl').hide();
+    //     $('.notification').fadeIn();
+    // });
 
     // Modal close
 
-	$('.intro_top__close').on('click', function () {
-		$('.intro').fadeOut();
-        $('.resources').fadeOut();
-        $('.notification').fadeOut();
-    });
+    // $('.intro_top__close').on('click', function () {
+		// $('.intro').fadeOut();
+    //     $('.resources').fadeOut();
+    //     $('.notification').fadeOut();
+    // });
 
 	// Undo
 
